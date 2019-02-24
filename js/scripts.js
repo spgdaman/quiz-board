706b52d8b0a690ad29ccf2dd35259932dbbd20b9
spgdaman <gatheru.simon432@gmail.com>
2019-02-24 22:17:48 +0300

    Get radio box input using JavaScript DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,46 @@
 $(document).ready(function () {
-  console.log("I work!");
+
   $("#submit").click(function () {
-    $("form").submit(function () {
-      var questionOneChoiceOne = $("#question-1#option-1:checked").value();
-      alert(questionOneChoiceOne);
-      alert("Hey there! I work as well!");
-    });
+
+    var questionOneChoiceOne = document.getElementbyId("#option-1");
+    var questionOneChoiceTwo = document.getElementbyId("#option-2");
+    var questionOneChoiceThree = document.getElementbyId("#option-3");
+    var questionOneChoiceFour = document.getElementbyId("#option-4");
+
+    var questionTwoChoiceOne = document.getElementbyId("#option-1");
+    var questionTwoChoiceTwo = document.getElementbyId("#option-2");
+    var questionTwoChoiceThree = document.getElementbyId("#option-3");
+    var questionTwoChoiceFour = document.getElementbyId("#option-4");
+
+    var questionThreeChoiceOne = document.getElementbyId("#option-1");
+    var questionThreeChoiceTwo = document.getElementbyId("#option-2");
+    var questionThreeChoiceThree = document.getElementbyId("#option-3");
+    var questionThreeChoiceFour = document.getElementbyId("#option-4");
+
+    var questionFourChoiceOne = document.getElementbyId("#option-1");
+    var questionFourChoiceTwo = document.getElementbyId("#option-2");
+    var questionFourChoiceThree = document.getElementbyId("#option-3");
+    var questionFourChoiceFour = document.getElementbyId("#option-4");
+
+    var answerQuestionOne = "123123";
+    var answerQuestionTwo = "Function literal";
+    var answerQuestionThree = "Stand-alone expressions";
+    var answerQuestionFour = "Invocation expression";
+    var answerQuestionFive = "Invocation expression";
+
+    console.log(questionOneChoiceOne);
+    confirm(questionOneChoiceOne);
+    return questionOneChoiceOne;
+
+    alert("Hey there! I work as well!");
+
+
+
 
   });
+
+  // console.log("I work!");
+  // $("form").hide();
 });
 
 //
