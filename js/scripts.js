3260430fa00b9c8fc5a7a39deb7f372d8aa065bb
spgdaman <gatheru.simon432@gmail.com>
2019-02-25 11:25:03 +0300

    Test if implementation works

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,44 @@
 $(document).ready(function () {
 
   $("#submit").click(function () {
-    alert("Hey there! I work as well!");
+    // alert("Hey there! I work as well!");
+    //
+    // var questionOneChoiceOne = document.getElementbyId("#option-1");
+    // var questionOneChoiceTwo = document.getElementbyId("#option-2");
+    // var questionOneChoiceThree = document.getElementbyId("#option-3");
+    // var questionOneChoiceFour = document.getElementbyId("#option-4");
+    //
+    // var questionTwoChoiceOne = document.getElementbyId("#option-1");
+    // var questionTwoChoiceTwo = document.getElementbyId("#option-2");
+    // var questionTwoChoiceThree = document.getElementbyId("#option-3");
+    // var questionTwoChoiceFour = document.getElementbyId("#option-4");
+    //
+    // var questionThreeChoiceOne = document.getElementbyId("#option-1");
+    // var questionThreeChoiceTwo = document.getElementbyId("#option-2");
+    // var questionThreeChoiceThree = document.getElementbyId("#option-3");
+    // var questionThreeChoiceFour = document.getElementbyId("#option-4");
+    //
+    // var questionFourChoiceOne = document.getElementbyId("#option-1");
+    // var questionFourChoiceTwo = document.getElementbyId("#option-2");
+    // var questionFourChoiceThree = document.getElementbyId("#option-3");
+    // var questionFourChoiceFour = document.getElementbyId("#option-4");
+    //
+    // var answerQuestionOne = "123123";
+    // var answerQuestionTwo = "Function literal";
+    // var answerQuestionThree = "Stand-alone expressions";
+    // var answerQuestionFour = "Invocation expression";
+    // var answerQuestionFive = "Invocation expression";
+    //
+    // console.log(questionOneChoiceOne);
+    // confirm(questionOneChoiceOne);
+    // return questionOneChoiceOne;
+    //
+    // alert("Hey there! I work as well!");
 
-    var questionOneChoiceOne = document.getElementbyId("#option-1");
-    var questionOneChoiceTwo = document.getElementbyId("#option-2");
-    var questionOneChoiceThree = document.getElementbyId("#option-3");
-    var questionOneChoiceFour = document.getElementbyId("#option-4");
-
-    var questionTwoChoiceOne = document.getElementbyId("#option-1");
-    var questionTwoChoiceTwo = document.getElementbyId("#option-2");
-    var questionTwoChoiceThree = document.getElementbyId("#option-3");
-    var questionTwoChoiceFour = document.getElementbyId("#option-4");
-
-    var questionThreeChoiceOne = document.getElementbyId("#option-1");
-    var questionThreeChoiceTwo = document.getElementbyId("#option-2");
-    var questionThreeChoiceThree = document.getElementbyId("#option-3");
-    var questionThreeChoiceFour = document.getElementbyId("#option-4");
-
-    var questionFourChoiceOne = document.getElementbyId("#option-1");
-    var questionFourChoiceTwo = document.getElementbyId("#option-2");
-    var questionFourChoiceThree = document.getElementbyId("#option-3");
-    var questionFourChoiceFour = document.getElementbyId("#option-4");
-
-    var answerQuestionOne = "123123";
-    var answerQuestionTwo = "Function literal";
-    var answerQuestionThree = "Stand-alone expressions";
-    var answerQuestionFour = "Invocation expression";
-    var answerQuestionFive = "Invocation expression";
+    var questionOneChoiceOne = $("input:radio[name=answer1]:checked").val();
 
     console.log(questionOneChoiceOne);
-    confirm(questionOneChoiceOne);
-    return questionOneChoiceOne;
-
-    alert("Hey there! I work as well!");
-
-
+    alert(questionOneChoiceOne);
 
 
   });
@@ -45,7 +48,7 @@ $(document).ready(function () {
 });
 
 //
-// var questionOneChoiceOne = $("#question-1 #option-1").value();
+// var questionOneChoiceOne = $("#question-1 #option-1").val();
 // // console.log("Hey! I work!");
 // // console.log(questionOneChoiceOne);
 // alert("I work too!");
